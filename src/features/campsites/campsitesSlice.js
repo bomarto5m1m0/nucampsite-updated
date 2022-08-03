7eecf8e594c9984dbe171299e666848afedf2b87
bomarto <bomarto@gmail.com>
2022-08-03 16:07:10 -0700

    week 4

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,7 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
+
 export const selectAllCampsites = () => {
-	return CAMPSITES;
+    return CAMPSITES;
 };
 
 export const selectCampsiteById = (id) => {
@@ -11,6 +23,7 @@ export const selectCampsiteById = (id) => {
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
 };
+
 
 // export const selectRandomCampsite = () => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
